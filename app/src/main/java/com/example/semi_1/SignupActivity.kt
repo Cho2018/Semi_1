f525835d265e07c05b2e220dffcaa1e3ad5cf44d
Cho2018 <hjchojane@naver.com>
2020-02-08 22:57:31 +0900

    1차

---
+++ app/src/main/java/com/example/semi_1/SignupActivity.kt
@@ -0,0 +1,41 @@
+package com.example.semi_1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val edtOnFocusChangeListener: View.OnFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+            if(hasFocus) v.setBackgroundResource(R.drawable.primary_border)
+            else v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        edtSignupName.setOnFocusChangeListener(edtOnFocusChangeListener)
+        edtSignupID.setOnFocusChangeListener(edtOnFocusChangeListener)
+        edtSignupPW.setOnFocusChangeListener(edtOnFocusChangeListener)
+
+        btnSignupSubmit.setOnClickListener {
+            val signup_u_name: String = edtSignupName.text.toString()
+            val signup_u_id = edtSignupID.text.toString()
+            val signup_u_pw: String = edtSignupPW.text.toString()
+
+            if(signup_u_name == "") edtSignupName.requestFocus()
+            else if(signup_u_id == "") edtSignupID.requestFocus()
+            else if(signup_u_pw == "") edtSignupPW.requestFocus()
+            else{
+                postSignupResponse(signup_u_id, signup_u_pw, signup_u_name)
+                finish()
+            }
+        }
+    }
+
+    fun postSignupResponse(u_id: String, u_pw: String, u_nmae: String){
+        finish()
+    }
+}
