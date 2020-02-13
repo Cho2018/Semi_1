700652ce86f1119bfccd3efc466e0b2b15c824ab
Cho2018 <hjchojane@naver.com>
2020-02-13 23:37:59 +0900

    3차_과제

---
+++ app/src/main/java/com/example/semi_1/Activity/WebtoonActivity.kt
@@ -0,0 +1,13 @@
+package com.example.semi_1.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.semi_1.R
+
+class WebtoonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webtoon)
+    }
+}
