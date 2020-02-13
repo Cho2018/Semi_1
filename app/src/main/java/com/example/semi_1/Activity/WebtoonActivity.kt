596071e542acfd0556737d06347c3d401ba5478a
Cho2018 <hjchojane@naver.com>
2020-02-14 01:03:07 +0900

    3차_과제

--- app/src/main/java/com/example/semi_1/Activity/WebtoonActivity.kt
+++ app/src/main/java/com/example/semi_1/Activity/WebtoonActivity.kt
@@ -3,11 +3,37 @@ package com.example.semi_1.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.semi_1.R
+import kotlinx.android.synthetic.main.toolbar_webtoon.*
+import org.jetbrains.anko.startActivity
 
 class WebtoonActivity : AppCompatActivity() {
+
+    lateinit var title: String
+    var product_id = -1
+    var episode_id = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webtoon)
+
+        configureTitleBar()
+    }
+
+    private fun configureTitleBar() {
+        title = intent.getStringExtra("title")
+        product_id = intent.getIntExtra("product_id", -1)
+        episode_id = intent.getIntExtra("episode_id", -1)
+
+        if(product_id == -1 || episode_id == -1) finish()
+
+        txt_toolbar_webtoon_title.text = title
+
+        btn_toolbar_webtoon_comment.setOnClickListener {
+            startActivity<CommentActivity>()
+        }
+
+        btn_toolbar_webtoon_back.setOnClickListener {
+            finish()
+        }
     }
 }
