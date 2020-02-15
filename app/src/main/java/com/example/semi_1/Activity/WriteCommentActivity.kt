9ff19efdf08da488f3d121390261d192dd3a6363
Cho2018 <hjchojane@naver.com>
2020-02-15 20:01:23 +0900

    5차 <생성> WriteCommentActivity.kt PostCommentResponse.kt activity_write_comment.xml toolbar_write_comment.xml bottom_write_comment.xml
    <수정>
    NetworkService.kt

--- app/src/main/java/com/example/semi_1/Activity/WriteCommentActivity.kt
+++ app/src/main/java/com/example/semi_1/Activity/WriteCommentActivity.kt
@@ -2,16 +2,38 @@ package com.example.semi_1.Activity
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bumptech.glide.Glide
+import com.example.semi_1.DB.SharedPreferenceController
+import com.example.semi_1.Network.ApplicationController
+import com.example.semi_1.Network.NetworkService
+import com.example.semi_1.Network.Post.PostCommentResponse
 import com.example.semi_1.R
 import kotlinx.android.synthetic.main.activity_write_comment.*
 import kotlinx.android.synthetic.main.bottom_write_comment.*
 import kotlinx.android.synthetic.main.toolbar_write_comment.*
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.jetbrains.anko.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.InputStream
 
 class WriteCommentActivity : AppCompatActivity() {
+
+    val networkService: NetworkService by lazy {
+        ApplicationController.instance.networkService
+    }
 
     lateinit var selectedPicUri: Uri
 
@@ -37,7 +59,9 @@ class WriteCommentActivity : AppCompatActivity() {
             finish()
         }
 
-        txt_toolbar_write_comment_submit.setOnClickListener {  }
+        txt_toolbar_write_comment_submit.setOnClickListener {
+            postCommentResponse()
+        }
 
         img_bottom_write_comment_camera.setOnClickListener {
             //Open Camera Roll
@@ -53,12 +77,58 @@ class WriteCommentActivity : AppCompatActivity() {
 
         if (requestCode == REQUEST_CODE_SELECT_IMAGE) {
             if (resultCode == Activity.RESULT_OK) {
-                data?.let { 
+                data?.let {
                     selectedPicUri = it.data
                     Glide.with(this).load(selectedPicUri)
                         .thumbnail(0.1f).into(img_comment_write_roll)
                 }
             }
+        }
+    }
+
+    fun isValid(token: String, comment: String): Boolean {
+        if (comment == "") edt_comment_write_comment.requestFocus()
+        else if (token == "") {
+            Log.e("token", "token failed")
+            return false
+        }
+        else return true
+        return false
+    }
+
+    private fun postCommentResponse() {
+        val token = SharedPreferenceController.getUserToken(this)
+        val comment = edt_comment_write_comment.text.toString()
+
+        if (isValid(token, comment)) {
+            val comment_rb = RequestBody.create(MediaType.parse("text/plain"), comment)
+
+            val options = BitmapFactory.Options()
+            val inputStream: InputStream = contentResolver.openInputStream(selectedPicUri)
+            val bitmap = BitmapFactory.decodeStream(inputStream, null, options)
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOutputStream)
+            val photoBody = RequestBody.create(MediaType.parse("image/jpg"), byteArrayOutputStream.toByteArray())
+
+            val picture_rb = MultipartBody.Part.createFormData("cmtImg", File(selectedPicUri.toString()).name, photoBody)
+
+            val postCommentResponse = networkService.postCommentResponse(token, episode_id, comment_rb, picture_rb)
+
+            postCommentResponse.enqueue(object: Callback<PostCommentResponse> {
+                override fun onFailure(call: Call<PostCommentResponse>, t: Throwable) {
+                    Log.e("write comment failed", t.toString())
+                }
+
+                override fun onResponse(call: Call<PostCommentResponse>, response: Response<PostCommentResponse>) {
+                    if (response.isSuccessful) {
+                        toast(response.body()!!.message)
+
+                        if (response.body()!!.status == 200) {
+                            finish()
+                        }
+                    }
+                }
+            })
         }
     }
 }
--- app/src/main/java/com/example/semi_1/Network/NetworkService.kt
+++ app/src/main/java/com/example/semi_1/Network/NetworkService.kt
@@ -1,9 +1,12 @@
 package com.example.semi_1.Network
 
 import com.example.semi_1.Network.Get.GetMainProductListResponse
+import com.example.semi_1.Network.Post.PostCommentResponse
 import com.example.semi_1.Network.Post.PostLoginResponse
 import com.example.semi_1.Network.Post.PostSignupResponse
 import com.google.gson.JsonObject
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -25,4 +28,14 @@ interface NetworkService {
         @Header("Content-Type") content_type: String,
         @Path("flag") flag: Int
     ): Call<GetMainProductListResponse>
+
+    @Multipart
+    @POST("/api/webtoons/episodes/cmts")
+    fun postCommentResponse(
+        @Header("token") token: String,
+
+        @Part("epIdx") epIdx: Int,
+        @Part("content") content: RequestBody,
+        @Part cmtImg: MultipartBody.Part
+    ): Call<PostCommentResponse>
 }
